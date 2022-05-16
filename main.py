85f5191cd923243bfb70b810b19a1ad8f3bb676a
gjaskula99 <81091594+gjaskula99@users.noreply.github.com>
2022-05-16 18:46:46 +0200

    Files IO implemented

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot, Qt
 
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QLineEdit, QMessageBox
-from PyQt5.QtWidgets import QLabel, QGridLayout, QWidget, QDesktopWidget, QTextEdit, QPushButton
+from PyQt5.QtWidgets import QLabel, QGridLayout, QWidget, QDesktopWidget, QTextEdit, QPushButton, QFileDialog
 
-import editorIO
 
 class App(QMainWindow):
 
@@ -30,16 +29,52 @@ class App(QMainWindow):
         self.textbox.resize(700, 500)
         self.textbox.setAlignment(Qt.AlignLeft)
 
+        def newFile():
+            self.textbox.setPlainText("")
+
+        def openFile():
+            filename = QFileDialog.getOpenFileName(self,'Open File')
+            if filename[0]:
+                f = open(filename[0],'r')
+                with f:
+                    data = f.read()
+                    self.textbox.setText(data)
+
+        def saveFile():
+            txt = self.textbox.toPlainText()
+            dlg = QFileDialog()
+            dlg.setFileMode(QFileDialog.AnyFile)
+            filenames = []
+            if(dlg.exec_()):
+                filenames = dlg.selectedFiles()
+                f = open(filenames[0], 'w')
+                f.write(txt)
+                f.close()
+
         #Toolbar
-        button = QPushButton('', self)
-        button.setStyleSheet("background-image : url(img/newfile.png);")
-        button.setToolTip('Create empty file')
-        button.resize(35, 35)
-        button.move(50,30)
-        button.clicked.connect(editorIO.newFile)
+        buttonNewFile = QPushButton('', self)
+        buttonNewFile.setStyleSheet("background-image : url(img/newfile.png);")
+        buttonNewFile.setToolTip('Create empty file')
+        buttonNewFile.resize(35, 35)
+        buttonNewFile.move(50,30)
+        buttonNewFile.clicked.connect(newFile)
+
+        buttonOpenFile = QPushButton('', self)
+        buttonOpenFile.setStyleSheet("background-image : url(img/openfile.png);")
+        buttonOpenFile.setToolTip('Open existing file')
+        buttonOpenFile.resize(35, 35)
+        buttonOpenFile.move(85,30)
+        buttonOpenFile.clicked.connect(openFile)
+
+        buttonOpenFile = QPushButton('', self)
+        buttonOpenFile.setStyleSheet("background-image : url(img/savefile.png);")
+        buttonOpenFile.setToolTip('Save current file')
+        buttonOpenFile.resize(35, 35)
+        buttonOpenFile.move(120,30)
+        buttonOpenFile.clicked.connect(saveFile)
 
         self.show()
-    
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
